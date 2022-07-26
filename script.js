43842a4dee20ce9a6cbcfa46e2265dd2192f9fc1
Fothe Ahmed <>
2022-07-26 14:58:12 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+// data type in javascript
+// 1: String --> "String"
+// 2: Number --> 10
+// 3: Booleans --> true or false
+// 4: Arrays --> ["string", 18, "string",[arrray]]
+// 5: Objects --> {"key": "value"}
+
+// operator in javascript 
+// assignment operators --> =
+// additional operators --> +
+// subtract operators --> -
+// multiply operators --> *
+// divided operators --> /
+// module operators --> %
+// exponent operators --> **
+
+// conditional statement in javascript 
+// if(condition){
+//   // code
+// }if else(codition){
+//   // code
+// }else{
+//   // code
+// }
+// logical operator in javascript
+// 1: and operators --> &&
+// 2: or operators --> ||
+// 3: not operators --> !
+// 4: not equal operators --> !=
+// 5: dobule equal --> ==
+// 6: triple equal --> ===
+
+// variables in javascript
+// var variables_name = "string or number";
+// ES6 Include let and const variable. this is updated for var keyword.
+// let name_variables = "string or number ";
+// const constent_variable = "string or number";
+
+// Project in javascript for make a button light on and off
+function on() {
+  document.getElementById("offLight").style.display = "none";
+}
+function off() {
+  document.getElementById("offLight").style.display = "block";
+}
